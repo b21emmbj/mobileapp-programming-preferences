a6240ec5a7099b0cf428defa95da00800b3bdb7e
b21emmbj <b21emmbj@student.his.se>
2022-05-03 14:29:11 +0200

    skapade layout med edittext + en knapp. La till i onResume att namnet "Emma" skrivs ut i första aktiviteten

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.project;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textviewname;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textviewname = findViewById(R.id.name);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", "Emma");
+        editor.apply();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
         String name = preferences.getString("name", "inget namn hittades");
+        textviewname.setText(name);
+
 
     }
 }
